a1a30ba3fbe9833fff98234093a6ecf652298476
Sivabharathi-dev <sivabharathi2005vs@gmail.com>
2025-02-10 15:25:46 +0530

    Add files via upload

---
+++ send whatsapp1.php
@@ -0,0 +1,25 @@
+﻿
+use Twilio\Rest\Client;
+
+function sendWhatsAppReminder($recipientNumber, $message) {
+    
+    $accountSid = '--Your twilio account Sid-- '; 
+    $authToken = '--Your twilio auth token--';  
+    $twilioNumber = 'whatsapp:+YOUR_TWILIO_WHATSAPP_NUMBER';  
+
+    $client = new Client($accountSid, $authToken);
+
+    try {
+        $client->messages->create(
+            'whatsapp:' . $recipientNumber, 
+            [
+                'from' => $twilioNumber,
+                'body' => $message
+            ]
+        );
+    } catch (Exception $e) {
+      
+        error_log('Error sending WhatsApp message: ' . $e->getMessage());
+    }
+}
+?>
